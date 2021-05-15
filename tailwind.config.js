cf9b2200c8a67ea617d59f7fe1bec8b43e2bea2d
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-05-15 16:55:08 -0500

    updated modules

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,tsx,hsx}', './components/**/*.{js, jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
